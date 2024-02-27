e8798d182e0ac0b2911b91d3254640e891d7beb7
liuxuejin <liuxujein@MacBook-Pro-6.local>
2024-02-27 19:52:05 +0800

    编写完成接口测试，服务器改为sanic

--- api/stock.py
+++ api/stock.py
@@ -1,8 +1,12 @@
-from http.server import BaseHTTPRequestHandler
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, jiebin!!!!!!!!!!'.encode('utf-8'))
-        return
+from sanic import Sanic
+from api.util import Util
+
+app = Sanic()
+
+@app.post('/stock')
+async def index(request, path=""):
+    start_date = request.form.get("startDate")
+    end_data = request.form.get("endDate")
+    name = request.form.get("name")
+    nlist = Util.get_percent(start_date, end_data, name)
+    return nlist
---
+++ api/util.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import yfinance as yf
+from datetime import datetime, timedelta
+
+
+class Util:
+    @staticmethod
+    def get_percent(start_data, end_data, name):
+        # CREATE TICKER INSTANCE FOR AMAZON
+        stock = yf.Ticker(name)
+        # GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
+        end_date = datetime.now().strftime('%Y-%m-%d')
+
+        date = datetime.strptime(start_data, "%Y-%m-%d")
+        new_date = date + timedelta(days=1)
+        new_date_str = new_date.strftime("%Y-%m-%d")
+        print(new_date.strftime("%Y-%m-%d"))
+        history = stock.history(start=start_data, end=new_date_str)
+        print(history)
+        df = pd.DataFrame(history, columns=['Open', 'High', 'Low', 'Close'])
+
+        start_open = 0
+        start_High = 0
+        start_Close = 0
+        start_Low = 0
+        for index, row in df.iterrows():
+            print('Index: ', index)
+            print('Open: ', row['Open'])
+            start_open = row['Open']
+            print('High: ', row['High'])
+            start_High = row['High']
+            print('Close: ', row['Close'])
+            start_Close = row['Close']
+            start_Low = row['Low']
+
+        date = datetime.strptime(end_date, "%Y-%m-%d")
+        new_date = date + timedelta(days=1)
+        new_date_str = new_date.strftime("%Y-%m-%d")
+        print(new_date.strftime("%Y-%m-%d"))
+        history = stock.history(start=end_data, end=new_date_str)
+        df = pd.DataFrame(history, columns=['Open', 'High', 'Low', 'Close'])
+        end_open = 0
+        end_High = 0
+        end_Close = 0
+        end_Low = 0
+        for index, row in df.iterrows():
+            print('Index: ', index)
+            print('Open: ', row['Open'])
+            end_open = row['Open']
+            print('High: ', row['High'])
+            end_High = row['High']
+            print('Close: ', row['Close'])
+            end_Close = row['Close']
+            end_Low = row['Low']
+
+        item = dict()
+        item['star_data'] = start_data
+        item['Open'] = f'%.2f' % start_open
+        item['High'] = f'%.2f' % start_High
+        item['Close'] = f'%.2f' % start_Close
+        item['Low'] = f'%.2f' % start_Low
+        nlist = []
+        list.append(item)
+        item1 = dict()
+        item1['end_data'] = end_data
+        item1['Open'] = f'%.2f' % end_open
+        item1['High'] = f'%.2f' % end_High
+        item1['Close'] = f'%.2f' % end_Close
+        item1['Low'] = f'%.2f' % end_Low
+        nlist.append(item1)
+        print(nlist)
+        return nlist
+
+
+Util.get_percent("2023-06-01", "2023-09-02", "AAPL")
